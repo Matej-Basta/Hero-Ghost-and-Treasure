b3cb2b692f5d1a891e902f09a75fe4b1a4ddb1ca
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-28 13:04:17 +0100

    Movement of the hero, with restraints

--- js/game.js
+++ js/game.js
@@ -1,11 +1,57 @@
-const hero = document.querySelector('.hero');
-hero.style.top = '500px';
-hero.style.left = '100px';
+const hero = document.querySelector(".hero");
+hero.style.top = "500px";
+hero.style.left = "100px";
 
-const ghost = document.querySelector('.ghost');
-ghost.style.top = '200px';
-ghost.style.left = '600px';
+const ghost = document.querySelector(".ghost");
+ghost.style.top = "200px";
+ghost.style.left = "600px";
 
-const treasure = document.querySelector('.treasure');
-treasure.style.top = '0px';
-treasure.style.left = '400px';
+const treasure = document.querySelector(".treasure");
+treasure.style.top = "0px";
+treasure.style.left = "400px";
+
+//variables used to add and subtract from the position
+let heroHorizontal = 100;
+let heroVertical = 500;
+//hero moving right
+document.addEventListener("keyup", (event) => {
+  if (
+    (event.code === "ArrowRight" || event.code === "KeyD") &&
+    heroHorizontal !== 700
+  ) {
+    heroHorizontal += 100;
+    hero.style.left = heroHorizontal + "px";
+  }
+});
+//hero moving left
+document.addEventListener("keyup", (event) => {
+  if (
+    (event.code === "ArrowLeft" || event.code === "KeyA") &&
+    heroHorizontal !== 0
+  ) {
+    heroHorizontal -= 100;
+    hero.style.left = heroHorizontal + "px";
+  }
+});
+//hero moving up
+document.addEventListener("keyup", (event) => {
+  if (
+    (event.code === "ArrowUp" || event.code === "KeyW") &&
+    heroVertical !== 0
+  ) {
+    heroVertical -= 100;
+    hero.style.top = heroVertical + "px";
+  }
+});
+//hero moving down
+document.addEventListener("keyup", (event) => {
+  if (
+    (event.code === "ArrowDown" || event.code === "KeyS") &&
+    heroVertical !== 500
+  ) {
+    heroVertical += 100;
+    hero.style.top = heroVertical + "px";
+  }
+});
+
+const announcingWictory = () => {};
